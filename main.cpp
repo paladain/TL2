9a49d0106ee69713b29bc3fea6f7d550527dc4fc
OnoMasachika <k021g1121@g.neec.ac.jp>
2023-10-03 15:30:53 +0900

    [02_03] 完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
+int main(int argc, char* argv[]) {
 
-	printf("Hello,world!\n");
+	//printf("Hello,world!\n");
+
+	// argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		// •¶š—ñ argv ‚Ì i ”Ô‚ğ•\¦
+		printf(argv[i]);
+		// ‰üs
+		printf("\n");
+	}
 
 	system("pause");
 
